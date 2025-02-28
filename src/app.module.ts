afc8b9f481050335d19884e86bbec9dfea2c3452
dinhvanbinh2310 <dinhvanbinh2310@gmail.com>
2025-02-28 20:19:13 +0700

    user register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketModule } from './ticket/ticket.module';
-
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/football-ticketing'),
     TicketModule,
+    UserModule,
   ],
 })
 export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { User, UserDocument } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { email, password, fullName } = createUserDto;
+
+    // Kiểm tra email đã tồn tại chưa
+    const existingUser = await this.userModel.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestException('Email đã được sử dụng');
+    }
+
+    // Mã hóa password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Tạo tài khoản mới
+    const newUser = new this.userModel({
+      email,
+      password: hashedPassword,
+      fullName,
+    });
+
+    return newUser.save();
+  }
+}
