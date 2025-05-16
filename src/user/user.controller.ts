cb9881bbe421681ee5210afea3bcdc1c8b70e432
dinhvanbinh2310 <dinhvanbinh2310@gmail.com>
2025-05-16 10:47:55 +0700

    feat: phân quyền user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,48 @@
-import { Controller, Post, Body, UseGuards, SetMetadata } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, SetMetadata, Put, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Get, Delete } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
+
   @Post('create-admin')
-  // @UseGuards(AuthGuard('jwt'), RolesGuard) // Bắt buộc đăng nhập và kiểm tra quyền
-  // @SetMetadata('role', 'admin')           // Chỉ cho phép admin thực hiện
-async createAdmin(@Body() body: { email: string; password: string }) {
-  return this.userService.createAdmin(body.email, body.password);
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
+  async createAdmin(@Body() body: { email: string; password: string }) {
+    return this.userService.createAdmin(body.email, body.password);
+  }
+
+  @Put(':id/role')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
+  async updateUserRole(
+    @Param('id') id: string,
+    @Body() body: { role: 'admin' | 'user' }
+  ) {
+    return this.userService.updateUserRole(id, body.role);
+  }
+  @Get()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
+  async getAllUsers() {
+    console.log('GET /user hit');
+    return this.userService.findAllUsers();
+  }
+  @Delete(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin') // nếu chỉ admin mới được xoá
+  async deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
 }
 
-}
